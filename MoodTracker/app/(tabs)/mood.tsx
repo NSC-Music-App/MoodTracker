baa9103df0c479b988fcd272d69af45b066ee605
6710110400 SASIT SUANSADANG <155415427+Sasit-Nine@users.noreply.github.com>
2025-03-16 00:56:53 +0700

    Add Mood Track UI and State and Edit Tabbar

--- MoodTracker/app/(tabs)/mood.tsx
+++ MoodTracker/app/(tabs)/mood.tsx
@@ -1,109 +1,276 @@
-import { StyleSheet, Image, Platform } from 'react-native';
+import { StyleSheet, Image, Platform, View, TextInput } from "react-native";
+import React from 'react';
+import { Collapsible } from "@/components/Collapsible";
+import { ExternalLink } from "@/components/ExternalLink";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import { Pressable, Text } from "react-native";
+import { useState } from "react";
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { IconSymbol } from '@/components/ui/IconSymbol';
 
 export default function MoodScreen() {
+    const [text,setText] = useState('')
+    const [emoji,setEmoji] = useState('')
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerBackgroundColor={{ light: "#FFFFFF", dark: "#353636" }}
       headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
+        <Image
+          source={require("@/assets/images/Earth and Moon-cuate.png")}
+          style={{
+            marginTop: 70,
+            width: 200,
+            height: 200,
+            alignSelf: "center",
+            resizeMode: "contain",
+          }}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Mood</ThemedText>
+        <ThemedText type="title">How are you today?</ThemedText>
       </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user's current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
+      {/* <IconSymbol size={32} style={{ marginTop: 25 }}/> */}
+      <ThemedView
+        style={{
+          flexDirection: "row", // ให้เรียงแนวนอน
+          flexWrap: "wrap", // เมื่อเต็มแถวจะไปแถวใหม่
+          justifyContent: "center", // จัดให้อยู่ตรงกลาง
+          alignItems: "center",
+          gap: 15, // ระยะห่างระหว่างปุ่ม
+        }}
+      >
+        <Pressable
+          onPress={()=>{setEmoji('Happy')}}
+          style={{
+            width: 70, // ขนาดของวงกลม
+            height: 70,
+            borderRadius: 35, // ทำให้เป็นวงกลม
+            backgroundColor: emoji === 'Happy' ? '#FFD700' : '#f0f0f0', 
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={require("@/assets/images/emoji/1.png")}
+            style={{
+              width: 60,
+              height: 60,
+              alignSelf: "center",
+              resizeMode: "contain",
+            }}
+          />
+        </Pressable>
+
+        <Pressable
+          onPress={()=>{setEmoji('Relaxed')}}
+          style={{
+            width: 70,
+            height: 70,
+            borderRadius: 35,
+            backgroundColor: emoji === 'Relaxed' ? '#FFD700' : '#f0f0f0', 
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={require("@/assets/images/emoji/2.png")}
+            style={{
+              width: 60,
+              height: 60,
+              alignSelf: "center",
+              resizeMode: "contain",
+            }}
+          />
+        </Pressable>
+
+        <Pressable
+          onPress={()=>{setEmoji('Neutral')}}
+          style={{
+            width: 70,
+            height: 70,
+            borderRadius: 35,
+            backgroundColor: emoji === 'Neutral' ? '#FFD700' : '#f0f0f0', 
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={require("@/assets/images/emoji/3.png")}
+            style={{
+              width: 60,
+              height: 60,
+              alignSelf: "center",
+              resizeMode: "contain",
+            }}
+          />
+        </Pressable>
+
+        <Pressable
+          onPress={()=>{setEmoji('Sad')}}
+          style={{
+            width: 70,
+            height: 70,
+            borderRadius: 35,
+            backgroundColor: emoji === 'Sad' ? '#FFD700' : '#f0f0f0', 
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={require("@/assets/images/emoji/4.png")}
+            style={{
+              width: 60,
+              height: 60,
+              alignSelf: "center",
+              resizeMode: "contain",
+            }}
+          />
+        </Pressable>
+
+        <Pressable
+          onPress={()=>{setEmoji('Angry')}}
+          style={{
+            width: 70,
+            height: 70,
+            borderRadius: 35,
+            backgroundColor: emoji === 'Angry' ? '#FFD700' : '#f0f0f0', 
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={require("@/assets/images/emoji/5.png")}
+            style={{
+              width: 60,
+              height: 60,
+              alignSelf: "center",
+              resizeMode: "contain",
+            }}
+          />
+        </Pressable>
+
+        {/* ปุ่มที่หก */}
+        <Pressable
+          onPress={()=>{setEmoji('Stressed')}}
+          style={{
+            width: 70,
+            height: 70,
+            borderRadius: 35,
+            backgroundColor: emoji === 'Stressed' ? '#FFD700' : '#f0f0f0', 
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={require("@/assets/images/emoji/6.png")}
+            style={{
+              width: 60,
+              height: 60,
+              alignSelf: "center",
+              resizeMode: "contain",
+            }}
+          />
+        </Pressable>
+
+        {/* ปุ่มที่เจ็ด */}
+        <Pressable
+          onPress={()=>{setEmoji('Mind-blown')}}
+          style={{
+            width: 70,
+            height: 70,
+            borderRadius: 35,
+            backgroundColor: emoji === 'Mind-blown' ? '#FFD700' : '#f0f0f0', 
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={require("@/assets/images/emoji/7.png")}
+            style={{
+              width: 60,
+              height: 60,
+              alignSelf: "center",
+              resizeMode: "contain",
+            }}
+          />
+        </Pressable>
+
+        {/* ปุ่มที่แปด */}
+        <Pressable
+          onPress={()=>{setEmoji('Sleepy')}}
+          style={{
+            width: 70,
+            height: 70,
+            borderRadius: 35,
+            backgroundColor: emoji === 'Sleepy' ? '#FFD700' : '#f0f0f0',
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={require("@/assets/images/emoji/8.png")}
+            style={{
+              width: 60,
+              height: 60,
+              alignSelf: "center",
+              resizeMode: "contain",
+            }}
+          />
+        </Pressable>
+      </ThemedView>
+      <ThemedText style={{fontSize:16, marginTop:10}}>Describe your feelings briefly.</ThemedText>
+      <ThemedView>
+        <TextInput
+          style={{
+            width: '100%',
+            height: 55,
+            borderColor: '#20A4F3',
+            borderWidth: 3,
+            borderRadius: 20,
+            paddingHorizontal: 10,
+            fontSize: 16,
+            marginBottom: 20,
+          }}
+          value={text}
+          onChangeText={setText} // อัปเดต state เมื่อกรอกข้อความ
+          placeholder="Enter your message here..." // ข้อความที่จะแสดงเมื่อยังไม่มีการกรอก
+        />
+      </ThemedView>
+      <ThemedView>
+        <Pressable
+          style={{
+            width: 180,
+            height: 55,
+            borderRadius: 25,
+            backgroundColor: "#20A4F3",
+            justifyContent: "center",
+            alignItems: "center",
+            alignSelf: "center"
+          }}
+        >
+            <Text style={{color:'white',fontSize:16}}>Generate</Text>
+        </Pressable>
+      </ThemedView>
+
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   headerImage: {
-    color: '#808080',
+    color: "#808080",
     bottom: -90,
     left: -35,
-    position: 'absolute',
+    position: "absolute",
   },
   titleContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     gap: 8,
+    marginTop: 5,
+    marginBottom:5,
+    alignSelf: "center",
   },
 });
